ddcbb15f17e930455ea629872e04a0ac526a123e
54mur41mx <c.albert15va@gmail.com>
2023-01-09 23:30:27 -0600

    Se sube prueba de codigo, se insertan cards nuevas con estilo responsivo desde localstorage

--- js/main.js
+++ js/main.js
@@ -63,7 +63,7 @@ function addItem3(item){
     const itemsContainer = document.getElementById("list-items3");
     itemsContainer.innerHTML += itemHTML;
 }
-
+/*
 addItem1({'name':'The Lord of the Rings', 'img':'https://m.media-amazon.com/images/I/51kfFS5-fnL._SX332_BO1,204,203,200_.jpg', 'description':'1954 The Lord of the Rings','price':'300'});
 
 addItem1({'name':'Lord of the Flies', 'img':'https://m.media-amazon.com/images/I/710kMF3SzEL.jpg', 'description':'1954 Lord of the Flies','price':'250'});
@@ -75,12 +75,30 @@ addItem2({'name':'Moby Dick', 'img':'https://m.media-amazon.com/images/I/91Dhazp
 addItem2({'name':'Frankenstein', 'img':'https://m.media-amazon.com/images/I/71f1FoY7DqL.jpg', 'description':'1831 Frankenstein','price':'350'});
 
 addItem2({'name':'The Count of Monte Cristo', 'img':'https://m.media-amazon.com/images/I/51fIrJcGO0L.jpg', 'description':'1846 The Count of Monte Cristo','price':'150'});
-
+/*
 addItem3({'name':'Dune', 'img':'https://m.media-amazon.com/images/I/81ym3QUd3KL.jpg', 'description':'1965 Dune','price':'150'});
 
 addItem3({'name':'Roadside Picnic', 'img':'https://m.media-amazon.com/images/I/51a5fMEvn2L._SY344_BO1,204,203,200_QL70_ML2_.jpg', 'description':'1972 Roadside Picnic','price':'150'});
 
 addItem3({'name':'The Stranger', 'img':'https://m.media-amazon.com/images/I/517eqjeVDzL._SX322_BO1,204,203,200_.jpg', 'description':'1942 The Stranger','price':'250'});
-/*
-addItem({'name':'Solaris', 'img':'https://m.media-amazon.com/images/I/31d1DpgbLfL._SY344_BO1,204,203,200_QL70_ML2_.jpg', 'description':'1961 Solaris','price':'50'});
 */
+
+let arreglolibros = [ {'name':'The Lord of the Rings', 'img':'https://m.media-amazon.com/images/I/51kfFS5-fnL._SX332_BO1,204,203,200_.jpg', 'description':'1954 The Lord of the Rings','price':'300'}, {'name':'Lord of the Flies', 'img':'https://m.media-amazon.com/images/I/710kMF3SzEL.jpg', 'description':'1954 Lord of the Flies','price':'250'},{'name':'The Great Gatsby', 'img':'https://m.media-amazon.com/images/I/51AMwVC+4xL._SY344_BO1,204,203,200_.jpg', 'description':'1925 The Great Gatsby','price':'500'}, {'name':'Moby Dick', 'img':'https://m.media-amazon.com/images/I/91DhazpQXML.jpg', 'description':'1851 Moby Dick','price':'300'}, {'name':'Frankenstein', 'img':'https://m.media-amazon.com/images/I/71f1FoY7DqL.jpg', 'description':'1831 Frankenstein','price':'350'},{'name':'Frankenstein', 'img':'https://m.media-amazon.com/images/I/71f1FoY7DqL.jpg', 'description':'1831 Frankenstein','price':'350'},{'name':'The Count of Monte Cristo', 'img':'https://m.media-amazon.com/images/I/51fIrJcGO0L.jpg', 'description':'1846 The Count of Monte Cristo','price':'150'}, {'name':'Dune', 'img':'https://m.media-amazon.com/images/I/81ym3QUd3KL.jpg', 'description':'1965 Dune','price':'150'},{'name':'Roadside Picnic', 'img':'https://m.media-amazon.com/images/I/51a5fMEvn2L._SY344_BO1,204,203,200_QL70_ML2_.jpg', 'description':'1972 Roadside Picnic','price':'150'},{'name':'The Stranger', 'img':'https://m.media-amazon.com/images/I/517eqjeVDzL._SX322_BO1,204,203,200_.jpg', 'description':'1942 The Stranger','price':'250'}];
+
+
+addItem3(JSON.parse(localStorage.getItem('libro')))
+
+
+var captura = JSON.parse(localStorage.getItem('libros'));
+
+for (let index = 0; index <= captura.length; index++) {
+    arreglolibros.push(captura[index]);
+    
+}
+
+for (let index = 0; index <= arreglolibros.length; index++) {
+    addItem3(arreglolibros[index])
+}
+
+
+
